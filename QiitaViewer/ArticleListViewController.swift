0c1d47292e7ae277f76364abeb64cff9bb687ef1
yuta-t <yutatuy.t@gmail.com>
2015-12-07 04:59:10 +0900

    Define getArticles method

--- QiitaViewer/ArticleListViewController.swift
+++ QiitaViewer/ArticleListViewController.swift
@@ -19,5 +19,23 @@ class ArticleListViewController: UIViewController {
         
         table.frame = view.frame
         view.addSubview(table)
+    func getArticles() {
+        Alamofire.request(.GET, "https://qiita.com/api/v2/items")
+            .responseJSON { response in
+                guard let object = response.result.value else {
+                    return
+                }
+                
+                let json = JSON(object)
+                json.forEach { (_, json) in
+                    let article: [String: String?] = [
+                        "title": json["title"].string,
+                        "userId": json["user"]["id"].string
+                    ]
+                    self.articles.append(article)
+                }
+                self.table.reloadData()
+        }
+    }
     }
 }
